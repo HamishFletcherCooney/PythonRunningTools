9b718d7f73c33edc48d8a1df2a6227361954a5c5
HamishFletcherCooney <65806510+HamishFletcherCooney@users.noreply.github.com>
2020-06-03 17:32:05 +0100

    Basic tool seperated into 3 basic functions and anotated.

--- UI_RunningTools.py
+++ UI_RunningTools.py
@@ -7,77 +7,90 @@ root.iconbitmap(r"C:\Users\Owner\Documents\GitHub\PythonRunningTools\runner.ico"
 
 """Program will create a window.
 Ask user for an existing race performance then use physiological formulas to calculate an equivalent performance for a 
-target distance"""
+target distance. Split into 3 functions: The users inputs; The layout of the inputs; The calculation using the Riegel 
+Formula"""
 
-myLabel_1 = Label(root, text="Recent race result")
-myLabel_1.grid(row=0,columnspan=3)
+#defines all variables given by user: previous distance & time. Target distance.
+def userinput():
+    #CompletedDistanceEntry
+    global ecd
+    ecd = Entry(root, width=10, borderwidth=5)
+    ecd.insert(0, "4") #4km in 13m50s is a recent race of mine, using as test, got bored of typing it in.
 
-myLabel_2 = Label(root, text="Distance")
-myLabel_2.grid(row=1,column=0)
+    #CompletedDistanceUnit
+    global u1
+    u1 = StringVar()
+    u1.set("km")
+    global cunit
+    cunit = OptionMenu(root, u1, "km","Miles","Meters")
 
-#CompletedDistanceEntry
-ecd = Entry(root,width=10,borderwidth=5)
-ecd.grid(row=1,column=1)
-ecd.insert(0, "completed distance") #4km in 13m50s is a recent race of mine, using as test, got bored of typing it in.
+    #CompletedDistanceTime
+    global ecth
+    ecth = Entry(root,width=10,borderwidth=5) #hours
+    ecth.insert(0, "0")
+    global ectm
+    ectm = Entry(root,width=10,borderwidth=5) #minutes
+    ectm.insert(0, "13")
+    global ects
+    ects= Entry(root,width=10,borderwidth=5) #seconds
+    ects.insert(0, "50")
 
-#CompletedDistanceUnit
-def show():
-    myLabel = Label(root, text=u1.get()).pack()
-u1 = StringVar()
-u1.set("km")
-drop = OptionMenu(root, u1, "km","Miles","Meters")
-drop.grid(row=1,column=2)
+    #TargetDistanceEntry
+    global etd
+    etd = Entry(root,width=10,borderwidth=5)
+    etd.insert(0, "TargetDistance")
 
-#CompletedDistanceTime
-ecth = Entry(root,width=10,borderwidth=5)
-ecth.grid(row=2,column=0)
-ecth.insert(0, "h")
+    #TargetDistanceUnit
+    global u2
+    u2= StringVar()
+    u2.set("km")
+    global tunit
+    tunit = OptionMenu(root, u2, "km","Miles","Meters")
 
-ectm = Entry(root,width=10,borderwidth=5)
-ectm.grid(row=2,column=1)
-ectm.insert(0, "mm")
+userinput()
 
-ects = Entry(root,width=10,borderwidth=5)
-ects.grid(row=2,column=2)
-ects.insert(0, "ss")
+#Layout of inputs
+def interface():
+    myLabel_1 = Label(root, text="Recent race result")
+    myLabel_1.grid(row=0,columnspan=3)
 
-myLabel_1 = Label(root, text="Target Race")
-myLabel_1.grid(row=3,columnspan=3)
+    myLabel_2 = Label(root, text="Distance")
+    myLabel_2.grid(row=1,column=0)
+    ecd.grid(row=1,column=1)
+    cunit.grid(row=1,column=2)
 
-#TargetDistanceEntry
-etd = Entry(root,width=10,borderwidth=5)
-etd.grid(row=4,column=1)
-etd.insert(0, "target distance")
+    ecth.grid(row=2,column=0)
+    ectm.grid(row=2,column=1)
+    ects.grid(row=2,column=2)
 
-myLabel_2 = Label(root, text="Distance")
-myLabel_2.grid(row=4,column=0)
+    myLabel_1 = Label(root, text="Target Race")
+    myLabel_1.grid(row=3,columnspan=3)
 
-#TargetDistanceUnit
-def show():
-    myLabel = Label(root, text=u2.get()).pack()
-u2 = StringVar()
-u2.set("km")
-drop = OptionMenu(root, u2, "km","Miles","Meters")
-drop.grid(row=4,column=2)
+    myLabel_2 = Label(root, text="Distance")
+    myLabel_2.grid(row=4, column=0)
+    etd.grid(row=4,column=1)
+    tunit.grid(row=4,column=2)
 
-#defining calculate button - it creates the t_time result
+interface()
+
+#defining calculate button - creates the target time result - 'prints' summary
 def button_calculate():
     completed_distance = ecd.get()
     global c_dist
     c_dist = float(completed_distance)
     global c_time
-    c_time = int(ecth.get())*3600 + int(ectm.get())*60 + int(ects.get())
+    c_time = int(ecth.get()) * 3600 + int(ectm.get()) * 60 + int(ects.get())
     global t_dist
     target_distance = etd.get()
     t_dist = float(target_distance)
     global t_time
-    t_time =0
+    t_time = 0
 
-#Converting all distances to km
+    # Converting all distances to km
     if u1.get() == "Miles":
-        c_dist = c_dist*1.60934
+        c_dist = c_dist * 1.60934
     if u1.get() == "Meters":
-        c_dist = c_dist/1000
+        c_dist = c_dist / 1000
     if u2.get() == "Miles":
         t_dist = t_dist * 1.60934
     if u2.get() == "Meters":
@@ -86,14 +99,14 @@ def button_calculate():
     t_time=c_time*(t_dist/c_dist)**1.06 #Formula to equate race times for different distances
 
 #Output
-    myLabel_1 = Label(root, text="Completed distance: " + str(c_dist) + " km")
-    myLabel_1.grid(row=5,columnspan=3)
-    myLabel_3 = Label(root, text=timedelta(seconds=int(c_time)))
-    myLabel_3.grid(row=6,columnspan=3)
-    myLabel_4 = Label(root, text="Target distance: " +str(t_dist) + " km")
-    myLabel_4.grid(row=7,columnspan=3)
-    myLabel_6 = Label(root, text=timedelta(seconds=int(t_time)))
-    myLabel_6.grid(row=8,columnspan=3)
+    myLabel_6 = Label(root, text="Completed distance: " + str(c_dist) + " km")
+    myLabel_6.grid(row=5,columnspan=3)
+    myLabel_7 = Label(root, text=timedelta(seconds=int(c_time)))
+    myLabel_7.grid(row=6,columnspan=3)
+    myLabel_8 = Label(root, text="Target distance: " +str(t_dist) + " km")
+    myLabel_8.grid(row=7,columnspan=3)
+    myLabel_9 = Label(root, text=timedelta(seconds=int(t_time)))
+    myLabel_9.grid(row=8,columnspan=3)
 
 Button_calculate = Button(root,text="Calculate equivalent time", command=button_calculate).grid(row=9,columnspan=3)
 
